3168aa0f1efbd0f91fb7ffd960661c4359ee5b98
Vishnu Vardhan Matcha <mvishnuvardhan048@gmail.com>
2024-12-25 16:38:05 +0530

    add java file

---
+++ src/main/java/mypkg/MyCalc.java
@@ -0,0 +1,21 @@
+package mypkg;
+
+public class MyCalc {
+	public int sum(int a , int b ) {
+	     return a+b;
+	}
+	public int diff(int a , int b ) {
+	     return a-b;
+	}
+
+	public static void main(String[] args) {
+		
+		
+		MyCalc ob= new MyCalc();
+		System.out.println("sum is"+ob.sum(10, 20));
+		System.out.println("sum is"+ob.diff(10, 20));
+		
+
+	}
+
+}
